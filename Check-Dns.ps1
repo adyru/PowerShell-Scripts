67ea4229414f1f4ded8c79135ff870fc71c1bfe5
adyru <42897327+adyru@users.noreply.github.com>
2020-06-14 08:49:24 +0100

    Add files via upload

--- Check-Dns.ps1
+++ Check-Dns.ps1
@@ -2,14 +2,21 @@
 Takes input of records from a text file of names
 Does look up writes to screen records
 or that it cant be found on server
+
+run command and then run teh below to find exception
+$Error[0].Exception.GetType().FullName
+System.ComponentModel.Win32Exception
+
 #>
 $Records = Get-Content servers.txt
 ForEach ($record in $Records)
     {
-    try{$Lookups = Resolve-DnsName $record -ErrorAction Stop}
-    catch [System.ComponentModel.Win32Exception] 
-            {
-    Write-host -ForegroundColor red $record " was not found"}
+    try
+            {$Lookups = Resolve-DnsName $record -ErrorAction Stop}
+   
+   catch [System.ComponentModel.Win32Exception] 
+            {Write-host -ForegroundColor red $record " was not found"}
+    
     Finally
             {
                forEach ($lookup in $Lookups)
