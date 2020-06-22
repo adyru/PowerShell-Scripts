9d8fa0c8f678214a541ab96c46a2012cd3ce430e
adyru <42897327+adyru@users.noreply.github.com>
2020-06-22 16:57:05 +0100

    Add files via upload

---
+++ Output-Process.ps1
@@ -0,0 +1,48 @@
+﻿
+<#
+this looks up what process has an open connection to a IP
+then outputs this to a csv
+#>
+#Change to ip you are after
+$IP = "*10*"
+$date = get-date -Format dd-MM-yyyy-HH-mm-ss
+$CSVOut =  $PSScriptRoot + "\" + $Date + "-Process.csv"
+#initialise array
+$PIDMatches = @()
+#Amend to how many ties to loop
+foreach($i in 1..5)
+    {
+    Write-Host $i
+    #how long to pause in each loop
+    Start-Sleep -Seconds 1
+    #here we look at what connections there are to an IP
+    $connections = Get-NetTCPConnection |? {$_.RemoteAddress -like $IP}
+        ForEach($connection in $connections)
+            {
+            #then we will look up the process that has that ip open
+            $processes = get-process -id $connection.OwningProcess
+            ForEach($process in $processes)
+                {
+                #for each of these will input to an array
+                    # we input what is useful into this
+                $process.ProcessName 
+                $process.ProcessName, $process.ID 
+                $PIDObject = New-Object System.Object
+                $PIDObject | Add-Member -type NoteProperty -name ProcessName -Value $process.ProcessName
+                $PIDObject | Add-Member -type NoteProperty -name PID  -Value $process.ID
+                $PIDObject | Add-Member -type NoteProperty -name RemoteAddress -Value $connection.RemoteAddress
+                $PIDObject | Add-Member -type NoteProperty -name RemotePort -Value $connection.RemotePort
+                $PIDObject | Add-Member -type NoteProperty -name CreationTime -Value $connection.CreationTime 
+                #add object to array
+                $Script:PIDMatches +=  $PIDObject
+                }
+            }
+
+    }
+#output this to a csv and null
+$PIDMatches | Export-Csv -NoClobber -NoTypeInformation -path $CSVOut
+$PIDMatches = $null
+
+
+
+
---
+++ SearchFiles.ps1
@@ -0,0 +1,42 @@
+﻿<#
+get all files on a computer
+and then se
+#>
+
+#Variables to change as required
+$date = get-date -Format dd-MM-yyyy-HH-mm-ss
+$output = $PSScriptRoot + "\" + $Date + "-output.txt"
+$errors = $PSScriptRoot + "\" + $Date + "-errors.txt"
+$search = "bdcadomc"
+$searchFolder = "C:\Users\adria\Documents\"
+
+write-host -ForegroundColor green " Search will be for " $search " in " $searchFolder
+
+#get files - exclude output,errors and scriptname so that it will only show ones we are intertested in
+$files = Get-ChildItem  $searchFolder -Recurse -File -ErrorAction SilentlyContinue -Exclude $output,$errors,$MyInvocation.MyCommand.Name
+
+
+foreach ($file in $files)
+#search through the files
+    {
+    #write-host "file is "$file
+        try
+            #try to look for the patten in a file and output it to file
+            {Select-String $file.fullname -Pattern $search -ErrorAction Stop | add-content $output}
+        
+   catch [System.ArgumentException] 
+            {
+            #if we get the locked file error output to host and write to file
+            Write-host -ForegroundColor yellow "error on file " $file.fullname " probably locked, will out put to errors file"
+            $file.fullname | add-content $errors
+            }
+    CATCH [System.Management.Automation.WildcardPatternException]
+            {
+            Write-host -ForegroundColor yellow "error on file " $file.fullname " generally because the file cant ne opened - will put in errorfile"
+            $file.fullname | add-content $errors
+            }
+            # we arent going to carry on with anything this time but might as well keep things as they should be in case i expand this
+    Finally
+            {
+            }
+    }
